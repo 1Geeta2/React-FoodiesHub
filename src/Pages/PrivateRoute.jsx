bba5ae3ee67253ca52b73ca7ab01cf7388eda662
1Geeta2 <geetanjaligunjal771@gmail.com>
2026-01-31 06:05:27 +0530

    Merged changes

--- src/Pages/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function PrivateRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("loggedIn");
-
-  return isLoggedIn ? children : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
